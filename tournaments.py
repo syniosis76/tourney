b606d1675f055487e0c0959c70d8074ec1bb5503
stacey <stacey.verner@ramm.com>
2018-09-25 12:35:02 +1200

    Closed connection.

--- tournaments.py
+++ tournaments.py
@@ -31,24 +31,27 @@ defaultData = [(uuid.uuid4(), '2019 Quarry Champs', datetime(2019, 1, 19), datet
 class tournaments:
     def on_get(self, req, resp, sort_order):        
         connection = db.open()
-        root = connection.root
+        try:            
+            root = connection.root
 
-        if hasattr(root, 'tournaments'):
-            print('Data is already stored.')
-            tournaments = root.tournaments
-        else:
-            print('Data is not already stored.')
-            tournaments = Tournaments()
-            root.tournaments = tournaments
-            for item in defaultData:
-                tournaments.addTournament(Tournament(item[0], item[1], item[2], item[3]))
-            transaction.commit()
+            if hasattr(root, 'tournaments'):
+                print('Data is already stored.')
+                tournaments = root.tournaments
+            else:
+                print('Data is not already stored.')
+                tournaments = Tournaments()
+                root.tournaments = tournaments
+                for item in defaultData:
+                    tournaments.addTournament(Tournament(item[0], item[1], item[2], item[3]))
+                transaction.commit()        
       
-        if sort_order == 'date':
-            sortedList = sorted(tournaments.list, key=lambda tournament: tournament.startDate)
-        else:
-            sortedList = sorted(tournaments.list, key=lambda tournament: tournament.name)
+            if sort_order == 'date':
+                sortedList = sorted(tournaments.list, key=lambda tournament: tournament.startDate)
+            else:
+                sortedList = sorted(tournaments.list, key=lambda tournament: tournament.name)
 
-        resp.body = json.dumps(list(map(lambda tournament: tournament.__dict__, sortedList)))
+            resp.body = json.dumps(list(map(lambda tournament: tournament.__dict__, sortedList)))
+        finally:
+            connection.close()
 
 api.add_route('/data/tournaments/{sort_order}', tournaments()) 
