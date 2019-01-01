7aa3ffa2812b27a763197796b4aac831d4ab0e24
Stacey Verner <stacey@verner.co.nz>
2019-01-01 19:01:59 +1300

    Updated style.

--- views/loginUser.js
+++ views/loginUser.js
@@ -2,7 +2,7 @@ export const loginUser = {
   template: `
 <div style="margin-left: 8px;">
   <router-link tag="a" to="/login">
-    <svg class="linkbutton" :class="{ selectedfill: googleUser.isSignedIn }"><use xlink:href="/html/icons.svg/#account"></use></svg>
+    <svg class="headerbutton" :class="{ selectedfill: googleUser.isSignedIn }"><use xlink:href="/html/icons.svg/#account"></use></svg>
   </router-link>
 </div>
 `,     
