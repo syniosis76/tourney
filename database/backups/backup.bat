607e21786794b0542c2074f4b3ae62e7a50cd0a4
Stacey Verner <stacey@verner.co.nz>
2019-04-17 19:42:35 +1200

    Added backup script.

---
+++ database/backups/backup.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set path=%path%;C:\Program Files\PostgreSQL\10\bin
+
+echo Backup Database
+call heroku pg:backups:capture -a cptourney HEROKU_POSTGRESQL_WHITE_URL
+
+echo
+echo Download Backup
+call heroku pg:backups:download -a cptourney
+
+echo
+echo Restore Backup
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -W aragorn1 -d tourney-dev latest.dump
+
+echo
+echo Copy to dated file
+ren latest.dump %date:~10,4%_%date:~4,2%_%date:~7,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%.dump
+
+pause
