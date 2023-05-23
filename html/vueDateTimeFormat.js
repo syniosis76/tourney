89cac254f46591f322deb7b4653bf052fb0995a4
Stacey Verner <stacey@verner.co.nz>
2023-05-23 20:51:18 +1200

    Update JS dependencies.

--- html/vueDateTimeFormat.js
+++ html/vueDateTimeFormat.js
@@ -4,30 +4,6 @@ Vue.filter('formatDate', function(value) {
   }
 })
 
-Vue.filter('formatDateTime', function(value) {
-  if (value) {
-    return moment(String(value)).format('MMM DD YYYY hh:mm')
-  }
-})
-
-Vue.filter('formatTime', function(value) {
-  if (value) {
-    return moment(String(value)).format('hh:mm')
-  }
-})
-
-Vue.filter('formatDay', function(value) {
-  if (value) {
-    return moment(String(value)).format('dddd')
-  }
-})
-
-Vue.filter('formatDayOfMonth', function(value) {
-  if (value) {
-    return moment(String(value)).format('dddd MMM DD')
-  }
-})
-
 Vue.filter('formatDayOfYear', function(value) {
   if (value) {
     return moment(String(value)).format('dddd MMM DD YYYY')
