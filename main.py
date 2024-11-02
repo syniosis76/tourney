f14735a4d7e25de3db5a38b796a71e9b82822583
Stacey Verner <stacey@verner.co.nz>
2024-11-02 19:17:29 +1300

    Debugging.

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ from routes import authentication
 if len(sys.argv) > 2:
     port = sys.argv[1]
     environment_variable = sys.argv[2]
+    print(f'{port} {environment_variable}')
     if environment_variable in os.environ:
         database_uri = os.environ[environment_variable]
+        print(database_uri)
         tourneyDatabase.tourneyDatabase.database_uri = database_uri
         serve(app, listen='*:' + port, threads=6)
     else:
