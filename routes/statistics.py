a242c7fe0d12be2306fe335c3fd743aba3c2c275
Stacey Verner <stacey@verner.co.nz>
2026-03-30 19:55:42 +1300

    Check for 11th-20th place suffixes in dependent games

--- routes/statistics.py
+++ routes/statistics.py
@@ -81,7 +81,8 @@ class Statistics:
     for team in teams:
       teamLower = team.lower()
       
-      placeSuffixes = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
+      placeSuffixes = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', 
+                       '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th']
       for suffix in placeSuffixes:
         if teamLower.endswith(suffix):
           return True
